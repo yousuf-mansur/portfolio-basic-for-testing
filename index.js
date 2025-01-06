a5eff74c0839d76e2d5e1caff6d4eaeaf3fbcf90
Md. Yousuf Mansur <mansurmdyousuf@gmail.com>
2025-01-06 20:50:10 +0600

    added images link

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // Smooth Scroll for Navigation
-document.querySelectorAll('nav ul li a').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
+document.querySelectorAll('nav ul li a').forEach(link => {
+  link.addEventListener('click', function (e) {
     e.preventDefault();
     const targetId = this.getAttribute('href').substring(1);
-    const targetElement = document.getElementById(targetId);
-    targetElement.scrollIntoView({ behavior: 'smooth' });
+    document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
   });
 });
 
@@ -116,3 +115,49 @@ style.innerText = `
     }
 `;
 document.head.appendChild(style);
+
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function (e) {
+  const formData = new FormData(form);
+  e.preventDefault();
+  var object = {};
+  formData.forEach((value, key) => {
+    object[key] = value;
+  });
+  var json = JSON.stringify(object);
+  result.innerHTML = 'Please wait...';
+
+  fetch('https://api.web3forms.com/submit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: json,
+  })
+    .then(async response => {
+      let json = await response.json();
+      if (response.status == 200) {
+        result.innerHTML = json.message;
+        result.classList.remove('text-gray-500');
+        result.classList.add('text-green-500');
+      } else {
+        console.log(response);
+        result.innerHTML = json.message;
+        result.classList.remove('text-gray-500');
+        result.classList.add('text-red-500');
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      result.innerHTML = 'Something went wrong!';
+    })
+    .then(function () {
+      form.reset();
+      setTimeout(() => {
+        result.style.display = 'none';
+      }, 5000);
+    });
+});
